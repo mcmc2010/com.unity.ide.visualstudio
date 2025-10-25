ca3975750ac2fc8072f814a90195c81777a1ecaa
unreal2012 <magicskyapple@gmail.com>
2025-10-25 18:40:49 +0800

    20250101-1
    Restore support for Visual Studio for Mac

--- Packages/com.unity.ide.visualstudio/Editor/Discovery.cs
+++ Packages/com.unity.ide.visualstudio/Editor/Discovery.cs
@@ -16,6 +16,9 @@ namespace Microsoft.Unity.VisualStudio.Editor
 #if UNITY_EDITOR_WIN
 			foreach (var installation in VisualStudioForWindowsInstallation.GetVisualStudioInstallations())
 				yield return installation;
+#elif UNITY_EDITOR_OSX
+			foreach (var installation in VisualStudioForMacInstallation.GetVisualStudioInstallations())
+				yield return installation;
 #endif
 
 			foreach (var installation in VisualStudioCodeInstallation.GetVisualStudioInstallations())
@@ -28,6 +31,9 @@ namespace Microsoft.Unity.VisualStudio.Editor
 			{
 #if UNITY_EDITOR_WIN
 				if (VisualStudioForWindowsInstallation.TryDiscoverInstallation(editorPath, out installation))
+					return true;
+#elif UNITY_EDITOR_OSX
+				if (VisualStudioForMacInstallation.TryDiscoverInstallation(editorPath, out installation))
 					return true;
 #endif
 				if (VisualStudioCodeInstallation.TryDiscoverInstallation(editorPath, out installation))
@@ -45,6 +51,8 @@ namespace Microsoft.Unity.VisualStudio.Editor
 		{
 #if UNITY_EDITOR_WIN
 			VisualStudioForWindowsInstallation.Initialize();
+#elif UNITY_EDITOR_OSX
+			VisualStudioForMacInstallation.Initialize();
 #endif
 			VisualStudioCodeInstallation.Initialize();
 		}
